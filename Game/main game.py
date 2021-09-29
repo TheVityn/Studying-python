2fa9adbe9da01fcf464e6c09d410f3e2cc6bbef4
TheVityn <78852822+TheVityn@users.noreply.github.com>
2021-09-29 11:00:25 +0200

    Pers. moving, centried

--- Game/main game.py
+++ Game/main game.py
@@ -8,22 +8,24 @@ def game():
             if event.type == pygame.QUIT:
                 pygame.quit()
                 sys.exit()
-        player.resize(0.2)
+        player1.resize(0.2)
 
         keys = pygame.key.get_pressed()
         if keys[pygame.K_RIGHT]:
-            player.moving(p_speed, 0)
+            player1.moving(p_speed, 0)
         elif keys[pygame.K_LEFT]:
-            player.moving(-p_speed, 0)
+            player1.moving(-p_speed, 0)
         if keys[pygame.K_UP]:
-            player.moving(0, -p_speed)
+            player1.moving(0, -p_speed)
         elif keys[pygame.K_DOWN]:
-            player.moving(0, p_speed)
+            player1.moving(0, p_speed)
 
         win.fill((127, 214, 84))
-        render(player, camera)
-        clock.tick(fps)
+
+        render(player1, camera)
+        render(ninja, camera)
         camera.update()
+        clock.tick(fps)
         pygame.display.update()
 
 pygame.init()
@@ -35,9 +37,10 @@ win = pygame.display.set_mode((winWidth, winHeight))
 pygame.display.set_caption('My RPG')
 pygame.display.set_icon(image)
 
-setscreen(win)
-player = Sprite(winWidth, winHeight, 'little_warior.png')
-camera = Camera(player)
+set_screen(win)
+player1 = Sprite(0, 0, 'little_warior.png')
+ninja = Sprite(0, 0, 'little_warior.png')
+camera = Camera(player1)
 fps = 60
 clock = pygame.time.Clock()
 
--- Game/sprites.py
+++ Game/sprites.py
@@ -1,9 +1,13 @@
 import pygame
 from PIL import Image
 
-def setscreen(window):
-    global screen
+def set_screen(window):
+    global screen, screen_w, screen_h
     screen = window
+    w, h = pygame.display.get_surface().get_size()
+    screen_w = w * 0.5
+    screen_h = h * 0.5
+
 
 class Camera:
     def __init__(self, object):
@@ -23,6 +27,8 @@ class Sprite:
         self.bitmap = pygame.image.load(filename)
         self.img = Image.open(filename)
         (self.img_width, self.img_height) = self.img.size
+        self.size_x = 0
+        self.size_y = 0
 
     def moving(self, x, y):
         self.x += x
@@ -30,12 +36,18 @@ class Sprite:
 
 
     def resize(self, size):
-        self.bitmap = pygame.transform.scale(self.bitmap, (int(self.img_width * size), int(self.img_height * size)))
+        self.size_x = int(self.img_width * size)
+        self.size_y = int(self.img_height * size)
+        self.bitmap = pygame.transform.scale(self.bitmap, (self.size_x, self.size_y))
+
 
 def render(self, camera):
-    if camera:
-        screen.blit(self.bitmap, (self.x - camera.x, self.y - camera.y))
-    else:
+    if camera == 'NONE':
         screen.blit(self.bitmap, (self.x, self.y))
+    else:
+        rend_x = self.x - camera.x + screen_w - self.size_x * 0.5
+        rend_y = self.y - camera.y + screen_h - self.size_y * 0.5
+        screen.blit(self.bitmap, (rend_x, rend_y))
+
 
 
